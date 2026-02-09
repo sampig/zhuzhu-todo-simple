b55fb3caeb435204bb2a097601e5ee50a3532af5
Chenfeng Zhu <zhuchenf@gmail.com>
2026-02-09 23:39:41 +0100

    Change the threshold for test coverage

--- components/TodoForm.test.js
+++ components/TodoForm.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { act } from 'react-test-renderer'
 import TodoForm from './TodoForm'
 
 describe('The TodoForm component', () => {
   it('renders without crashing', () => {
-    const render = renderer.create(<TodoForm addNewTodo={jest.fn()} />).toJSON()
+    let render
+    act(() => {
+      render = renderer.create(<TodoForm addNewTodo={jest.fn()} />).toJSON()
+    })
     expect(render).toMatchSnapshot()
   })
 })
